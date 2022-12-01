52f0ab76bc59c0083cd739f0a312938018924308
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-02 00:02:15 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -34,13 +34,13 @@
 //	return 0;
 //}
 
-#include<stdio.h>
-int main()
-{
-	int num1 = 0;
-	int num2 = 0;
-	scanf("%d%d", &num1, &num2);
-	int sum = num1 + num2;
-	printf("%d\n", sum);
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d", &num1, &num2);
+//	int sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
