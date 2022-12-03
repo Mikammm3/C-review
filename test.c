37943fc5ea41742a192afc2bdac762b74520e5cd
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-03 13:33:35 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,46 +1,84 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
-
 //#include<stdio.h>
 //
-////主函数  ---程序主入口
-////有且只有一个
+////全局变量
+//int b = 20;
+//
+////声明外部符号
+//extern int g_val;
+//
 //int main()
 //{
-//	printf("hello world\n");
+//	//局部变量
+//	int a = 10;
+//	printf("%d\n", g_val);
+//	return 0;
+//}
+
+
+////#define定义的标识符常量
+//#define MAX 100
+//
+////枚举常量
+//enum SEX
+//{
+//	MALE,//   0
+//	FEMALE=5,// 5
+//	SECRET//  6
+//};
+//
+//#include<stdio.h>
+//int main()
+//{
+//	//字面常量
+//	100;
+//
+//	//const修饰的常变量
+//	//本质上还是个变量
+//	const int num = 100;
+//	num = 200;//err
+//
+//	int arr[MAX] = { 0 };
+//	return 0;
+//}
+
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	//"abc"--  'a'  'b'  'c'  '\0' 
+//	char arr1[] = "abc";//隐藏了一个 \0
+//	//数组：同一类型的变量的集合
+//	char arr2[] = { 'a','b','c' };
+//	//strlen是计算字符串长度，遇到\0停下来，计算\0之前的字符个数
+//	printf("%d\n", strlen(arr1));//strlen是库函数，使用前要引用string.h
+//	printf("%d\n", strlen(arr2));
 //	return 0;
 //}
 
 //#include<stdio.h>
 //int main()
 //{
-//	//%d 是打印整数，sizeof是计算类型的大小
-//	printf("%d\n", sizeof(char));
-//	printf("%d\n", sizeof(short));
-//	printf("%d\n", sizeof(int));
-//	printf("%d\n", sizeof(long));
-//	printf("%d\n", sizeof(long long));
-//	printf("%d\n", sizeof(float));
-//	printf("%d\n", sizeof(double));
+//	int input = 0;
+//	printf("你要好好学习吗（1/0）\n");
+//	scanf("%d", &input);
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
 //	return 0;
 //}
 
 //#include<stdio.h>
 //int main()
 //{
-//	int age = 20;
-//	char ch = 'm';
-//	float f = 3.14f;
-//	return 0;
-//}
-
-//#include<stdio.h>
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	scanf("%d%d", &num1, &num2);
-//	int sum = num1 + num2;
-//	printf("%d\n", sum);
+//	int line = 0;
+//	printf("加入比特\n");
+//	while (line < 20000)
+//	{
+//		printf("敲代码%d\n", line);
+//		line++;
+//	}
 //	return 0;
 //}
