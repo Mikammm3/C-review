338fa4ca5868d8ab07248c89b2e256987ae3431a
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-22 18:40:12 +0800

    Add files via upload

---
+++ exercise_12_22.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include<string.h>
+
+
+//int main()
+//{
+//    char a1[100], a2[100];
+//    scanf("%s %s", a1, a2);
+//    if (strcmp(a1, a2) == 0)
+//        printf("same\n");
+//    else
+//        printf("different\n");
+//    return 0;
+//}
+
+//int main()
+//{
+//    int arr[10] = { 0 };
+//    for (int i = 0; i < 10; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (int i = 9; i >= 0; i--)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int n = 0;
+//    int arr[50] = { 0 };
+//    scanf("%d", &n);
+//    int sum = 0;
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//        sum += arr[i];
+//    }
+//    printf("%d\n", sum);
+//    return 0;
+//}
+
+
