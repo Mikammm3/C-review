49ead4f7347f8abc2102cd0cd36a4e02e2ee63a2
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-27 19:36:51 +0800

    Add files via upload

---
+++ exercise_12_27.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int a, b, c, d;
+//    scanf("%d %d %d %d", &a, &b, &c, &d);
+//    printf("%.1f", a * 0.2 + b * 0.1 + c * 0.2 + d * 0.5);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    for (int i = 0; i < n; i++)
+//    {
+//        printf("Happy new year!Good luck!\n");
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int a, b, c;
+//    scanf("%d %d %d", &a, &b, &c);
+//    int max = 0;
+//    max = (a > b ? a : b);
+//    max = (max > c ? max : c);
+//    printf("%d\n", max);
+//    return 0;
+//}
