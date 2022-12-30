d15625a270f2aeb0f053cacb17ca2bf2f79e06db
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-30 20:06:12 +0800

    Add files via upload

---
+++ exercise_12_30.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+
+
+
+//void PrintN(int n)
+//{
+//    int i = 0;
+//    for (i = 1; i <= n; i++)
+//    {
+//        printf("%d\n", i);
+//    }
+//}
+
+
+//int Sum(int List[], int N)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < N; i++)
+//    {
+//        sum += List[i];
+//
+//    }
+//    return sum;
+//}
+
+
+//double f(int n, double a[], double x)
+//{
+//    double ret = 0;
+//    double sum = 0;
+//    for (int i = 0; i <= n; i++)
+//    {
+//        ret = a[i] * pow(x, i);
+//        sum += ret;
+//    }
+//    return sum;
+//}
+
+
+//int main()
+//{
+//    int a = 0;
+//    scanf("%d", &a);
+//    while (a)
+//    {
+//        printf("%d", a % 10);
+//        a /= 10;
+//    }
+//    return 0;
+//}
+
+
+
+//int main()
+//{
+//    int ch = 0;
+//    while ((ch = getchar()) != EOF)
+//    {
+//        printf("%c\n", ch + 32);
+//        getchar();
+//    }
+//    return 0;
+//}
