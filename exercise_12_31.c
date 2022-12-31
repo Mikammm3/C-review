4a16e31a08caeb2f13fa8d8d546bf015dc4325e8
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2022-12-31 20:57:15 +0800

    Add files via upload

---
+++ exercise_12_31.c
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//ElementType Average(ElementType S[], int N)
+//{
+//    int i = 0;
+//    ElementType sum = 0;
+//    for (i = 0; i < N; i++)
+//    {
+//        sum += S[i];
+//    }
+//    return sum / N;
+//}
+
+
+
+//ElementType Max(ElementType S[], int N)
+//{
+//    ElementType max = -10000;
+//    for (int i = 0; i < N; i++)
+//    {
+//        if (max < S[i])
+//            max = S[i];
+//    }
+//    return max;
+//}
+
+
+//#include <stdio.h>
+//#include<ctype.h>
+//int main()
+//{
+//    char ch = 0;
+//    scanf("%c", &ch);
+//    if (isalpha(ch) != 0)
+//        printf("YES\n");
+//    else
+//        printf("NO\n");
+//    return 0;
+//}
+
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int ch = 0;
+//    ch = getchar();
+//    int i = 0;
+//    for (i = 0; i < 5; i++)
+//    {
+//        for (int j = 0; j < 4 - i; j++)
+//        {
+//            printf(" ");
+//        }
+//        for (int j = 0; j <= i; j++)
+//        {
+//            printf("%c ", ch);
+//        }
+//        printf("\n");
+//    }
+//    return 0;
+//}
+
+
+
+#include <stdio.h>
+#include<math.h>
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    printf("%d\n", 1 << n);
+//    return 0;
+//}
