893c02ddafb39e1c695f9032409ff5dd813dff29
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-01-17 22:08:13 +0800

    Add files via upload

---
+++ exercise_2023_1_17.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int i = 0;
+//    int sum = 0;
+//    int count = 0;
+//    for (i = 2;; i += 3)
+//    {
+//        sum += i;
+//        count++;
+//        if (count == n)
+//            break;
+//    }
+//    printf("%d", sum);
+//    return 0;
+//}
+
+
+#include <stdio.h>
+
+//int main()
+//
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int start = n * (n - 1) + 1;
+//    int i = 0;
+//    printf("%d", start);
+//    for (i = 1; i < n; i++)
+//    {
+//        start += 2;
+//        printf("+%d", start);
+//    }
+//    return 0;
+//}
