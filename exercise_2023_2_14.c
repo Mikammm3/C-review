03b4cb902793d655a7df32eb1b74a36cb50b1342
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-02-14 14:55:54 +0800

    Add files via upload

---
+++ exercise_2023_2_14.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//    double pi = 1.0, sum = 1.0, ret, t;
+//    scanf("%lf", &t);
+//    int i = 1;
+//    while (pi > t)
+//    {
+//        pi = pi * i / (2 * i + 1);
+//        sum += pi;
+//        i++;
+//    }
+//    printf("%.6lf\n", sum * 2);
+//    return 0;
+//}
