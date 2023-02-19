fd4aac8b2137403a6ab6dacd60f32b279b0aad7a
Mikammm3 <w2059909086@gmail.com>
2023-02-19 19:50:57 +0800

    exercise daily

---
+++ exercise/exercise/exercise_2023_2_19.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int flag = 0;
+//    for (int y = 0; y < 100; y++)
+//    {
+//        for (int f = 0; f < 100; f++)
+//        {
+//            if (200 * y + 2 * f == f * 100 + y - n)
+//            {
+//                printf("%d.%d\n", y, f);
+//                flag++;
+//                break;
+//            }
+//        }
+//    }
+//    if (flag == 0)
+//        printf("No Solution\n");
+//    return 0;
+//}
