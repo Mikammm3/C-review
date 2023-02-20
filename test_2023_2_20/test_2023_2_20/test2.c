53cd98e5c84a5d49410ab0280e1ccdb717a510fd
Mikammm3 <w2059909086@gmail.com>
2023-02-20 22:31:13 +0800

    learn C

---
+++ test_2023_2_20/test_2023_2_20/test2.c
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i + j);
+//	//答案：-10，因为打印的是有符号整数
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	unsigned int i;
+//	for (i = 9; i >= 0; i--)
+//	{
+//		printf("%u\n", i);
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//
+//
+//	char a[1000];
+//	int i;
+//	for (i = 0; i < 1000; i++)
+//	{
+//		a[i] = -1 - i;
+//	}
+//	printf("%d", strlen(a));//找'\0'
+//	//char -128~127
+//	//-1 -2.... -128 127 126 ... 3 2 1 0
+//	//128+127=255
+//	return 0;
+//}
+
+
+
+
+
+//#include <stdio.h>
+//unsigned char i = 0;
+//int main()
+//{
+//	for (i = 0; i <= 255; i++)
+//	{
+//		printf("hello world\n");//死循环
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 9;
+//	float* pFloat = (float*)&n;
+//	printf("n的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	*pFloat = 9.0;
+//	printf("num的值为：%d\n", n);
+//	printf("*pFloat的值为：%f\n", *pFloat);
+//	return 0;
+//}
+
+
+
