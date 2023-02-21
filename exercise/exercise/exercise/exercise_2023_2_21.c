69f88d50925b7e92306168c797d9ef6b5d606cf2
Mikammm3 <w2059909086@gmail.com>
2023-02-21 21:14:27 +0800

    exercise daily

---
+++ exercise/exercise/exercise/exercise_2023_2_21.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//    char ch;
+//    int flag = 0;
+//    while (1)
+//    {
+//        int cnt = 0;
+//        scanf("%c", &ch);
+//        while (ch != '\n' && ch != ' ' && ch != '.')
+//        {
+//            cnt++;
+//            scanf("%c", &ch);
+//        }
+//        if (cnt != 0)
+//        {
+//            if (flag == 0)
+//            {
+//                printf("%d", cnt);
+//                flag++;
+//            }
+//            else
+//            {
+//                printf(" %d", cnt);
+//            }
+//        }
+//        if (ch == '.')
+//            break;
+//    }
+//}
