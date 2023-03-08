547c4fe36a028227867f41484a2f8783b90d7505
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-08 19:46:15 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_8.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//    int arr[10] = { 0 };
+//    char str[1000] = { 0 };
+//    scanf("%s", str);
+//    for (int i = 0; i < strlen(str); i++)
+//    {
+//        int ret = str[i] - '0';
+//        arr[ret]++;
+//    }
+//    for (int i = 0; i < 10; i++)
+//    {
+//        if (arr[i] > 0)
+//        {
+//            printf("%d:%d\n", i, arr[i]);
+//        }
+//    }
+//    return 0;
+//}
