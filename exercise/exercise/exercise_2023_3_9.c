de281e4a8c21ff941816c93aadad7a8ce9a03928
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-09 14:39:33 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_9.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//int main()
+//{
+//    int F = 0;
+//    scanf("%d", &F);
+//    printf("Celsius = %d\n", 5 * (F - 32) / 9);
+//    return 0;
+//}
