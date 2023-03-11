d256d5e7ae4fe2da3a4ed8cc49f4bc1c499b560f
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-11 20:40:51 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_11.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    if (n < 0)
+//    {
+//        printf("fu ");
+//        n = -n;
+//    }
+//    int tmp = n;
+//    int count = 1;
+//    while (tmp /= 10)
+//    {
+//        count++;
+//    }
+//    char* p[10] = { "ling","yi","er","san","si",
+//                 "wu","liu","qi","ba","jiu" };
+//    for (int i = pow(10, count - 1); i > 0; i /= 10)
+//    {
+//        int ret = n / i % 10;
+//        if (i >= 10)
+//            printf("%s ", p[ret]);
+//        else
+//            printf("%s", p[ret]);
+//    }
+//    return 0;
+//}
