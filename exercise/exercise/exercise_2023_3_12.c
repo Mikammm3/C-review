86bc8efe626f6c7d25824222137b6a33fef8ccd5
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-12 20:46:22 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_12.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//    int arr1[10][10] = { 0 };
+//    int arr2[10][10] = { 0 };
+//    int n = 0;
+//    int m = 0;
+//    scanf("%d %d", &n, &m);
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            scanf("%d", &arr1[i][j]);
+//        }
+//    }
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            scanf("%d", &arr2[i][j]);
+//        }
+//    }
+//    int flag = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        int j = 0;
+//        for (j = 0; j < m; j++)
+//        {
+//            if (arr1[i][j] != arr2[i][j])
+//            {
+//                flag = 1;
+//                printf("No\n");
+//                i += n;
+//            }
+//        }
+//    }
+//    if (flag == 0)
+//        printf("Yes\n");
+//    return 0;
+//}
+
+
+
+
