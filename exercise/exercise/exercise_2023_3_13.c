961e5a3bd3964587a3d8a79345ec3e71fb3b2445
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-13 23:20:44 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_13.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+////Ä£ÄâÊµÏÖstrlen
+//
+//#include <stdio.h>
+//#include <assert.h>
+//int my_strlen(const char* str)
+//{
+//	assert(str);
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
+
+
