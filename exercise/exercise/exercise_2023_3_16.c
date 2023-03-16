32d5357ca2b1fe442c5e1e655a32d079d59d34f6
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-16 20:09:38 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_16.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    int sum = 0;
+//    int count = 0;
+//    for (int i = a; i <= b; i++)
+//    {
+//        count++;
+//        printf("%5d", i);
+//        if (count % 5 == 0 && b - a != 4)
+//            printf("\n");
+//        sum += i;
+//    }
+//    printf("\nSum = %d\n", sum);
+//    return 0;
+//}
