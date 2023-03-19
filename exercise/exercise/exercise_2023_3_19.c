5ae8ec91ee256a69bc88afbcd7bc3a9e9d92dba3
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-19 20:11:34 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_19.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    scanf("%d %d %d", &a, &b, &c);
+//    int max = (a > b ? a : b);
+//    int min = (a < b ? a : b);
+//    max = (max > c ? max : c);
+//    min = (min < c ? min : c);
+//    printf("%d->", min);
+//    if (a != max && a != min)
+//        printf("%d", a);
+//    else if (b != max && b != min)
+//        printf("%d", b);
+//    else
+//        printf("%d", c);
+//    printf("->%d\n", max);
+//    return 0;
+//}
