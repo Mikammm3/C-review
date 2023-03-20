415dfd00b1832c55a976b6cf28185943eb633463
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-20 21:49:44 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_20.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//
+//int FactorSum(int n)
+//{
+//    int i = 0;
+//    int ret = 1;
+//    int sum = 0;
+//    for (i = 1; i <= n; i++)
+//    {
+//        ret *= i;
+//        sum += ret;
+//    }
+//    return sum;
+//}
+//
+//int main()
+//{
+//    int N = 0;
+//    scanf("%d", &N);
+//    int ret = FactorSum(N);
+//    printf("%d\n", ret);
+//    return 0;
+//}
