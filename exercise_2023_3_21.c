d6b394f6e255d8d0a5a78639cc7940d21a71d6f9
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-21 22:32:24 +0800

    Add files via upload

---
+++ exercise_2023_3_21.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("I'm gonna WIN!\n");
+//    printf("I'm gonna WIN!\n");
+//    printf("I'm gonna WIN!\n");
+//    return 0;
+//}
