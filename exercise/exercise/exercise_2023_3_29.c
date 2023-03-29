cd0c028e4c617f735db198a78355e579517fd87b
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-03-29 16:34:31 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_3_29.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+//#include <stdio.h>
+//
+//struct S
+//{
+//	int n;
+//	float f;
+//	char arr[20];
+//};
+//
+//int main()
+//{
+//	struct S s = { 100,6.66,"hehe" };
+//	char arr[200] = { 0 };
+//	//将格式化数据转为字符串
+//	sprintf(arr, "%d %f %s\n", s.n, s.f, s.arr);
+//	printf("%s\n", arr);
+//	struct S tmp = { 0 };
+//	//将字符串转为格式化数据
+//	sscanf(arr, "%d %f %s", &(tmp.n), &(tmp.f), tmp.arr);
+//	printf("%d %f %s\n", tmp.n, tmp.f, tmp.arr);
+//	return 0;
+//}
+
