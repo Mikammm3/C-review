18873cb94881f87dd1f7b6c214b7bd26598b9665
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-04 11:03:28 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include <stdio.h>
+
+//函数的定义
+//int Max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//
+////宏定义
+//#define MAX(X,Y) ((X)>(Y)?(X):(Y))
+//
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int m1 = Max(a, b);
+//	printf("%d\n", m1);
+//	int m2 = MAX(a, b);
+//	printf("%d\n", m2);
+//	return 0;
+//}
+
+
+//#define MALLOC(num,type)  ((type*)malloc((num)*sizeof(type)))
+//
+//int main()
+//{
+//	int* p = MALLOC(10, int);
+//
+//	return 0;
+//}
+
+
+//#define M 100
+//
+//int main()
+//{
+//	printf("%d\n", M);
+//
+//#undef M
+//	printf("%d\n", M);
+//
+//	return 0;
+//}
+
+
+
