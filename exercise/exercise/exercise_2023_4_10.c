f1768866886952e95a04673830b096d652007264
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-10 19:38:31 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_4_10.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int h = 0;
+//    int m = 0;
+//    scanf("%d:%d", &h, &m);
+//    if (h >= 0 && h <= 12)
+//    {
+//        printf("Only %02d:%02d.  Too early to Dang.\n", h, m);
+//    }
+//    else
+//    {
+//        for (int i = 0; i < h - 12; i++)
+//        {
+//            printf("Dang");
+//        }
+//        if (m != 0)
+//            printf("Dang");
+//    }
+//    return 0;
+//}
