c330db4b2b3d8777e75f912e07a1dc6ded15df7b
Mikammm3 <115018037+Mikammm3@users.noreply.github.com>
2023-04-12 23:20:01 +0800

    Add files via upload

---
+++ exercise/exercise/exercise_2023_4_12.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    int d = 0;
+//    scanf("%d", &d);
+//    if ((d + 2) % 7 == 0)
+//    {
+//        printf("7\n");
+//        return 0;
+//    }
+//    d = (d + 2) % 7;
+//    printf("%d\n", d);
+//    return 0;
+//}
+
+
+
+//#include <stdio.h>
+//
+//int main()
+//{
+//    printf("I\n \nL\no\nv\ne\n \nG\nP\nL\nT\n");
+//    return 0;
+//}
